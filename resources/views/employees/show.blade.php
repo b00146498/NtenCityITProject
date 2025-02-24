f569f2b4c9f3e53acca5ccd6326b389f1354a356
b00146498@mytudublin.ie <b00146498@mytudublin.ie>
2025-02-24 20:48:03 +0000

    added styling and forms to view client and employee details

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -1,19 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="content-header">
-        <h1>
-            View Employee Details
-        </h1>
-    </section>
-    <div class="content">
-        <div class="box box-primary">
-            <div class="box-body">
-                <div class="row" style="padding-left: 20px">
-                    @include('employees.show_fields')
-                    <a href="{!! route('employees.index') !!}" class="btn btn-default">Back</a>
-                </div>
+<div class="container mt-1">
+    <div class="card shadow-lg">
+        <div class="card-header bg-primary text-white d-flex justify-content-between">
+            <h4>View Employee Details</h4>
+            <a href="{{ route('employees.index') }}" class="text-white text-decoration-none">✖ Close</a>
+        </div>
+        <div class="card-body">
+            @include('employees.show_fields')
+
+            <div class="d-flex justify-content-end mt-3">
+                <a href="{!! route('employees.index') !!}" class="btn btn-outline-dark px-4 py-2">Back</a>
             </div>
         </div>
     </div>
+</div>
 @endsection
+
