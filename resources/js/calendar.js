71ba55210b0af4c377a1badb31396f772c6fb943
B00144945 <B00144945@mytudublin.ie>
2025-03-03 14:45:26 +0000

    changed default calendar view to weeks

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -2,10 +2,14 @@ import * as Calendar from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
-import interactionPlugin from '@fullcalendar/interaction';
+import interactionPlugin from '@fullcalendar/interaction'; 
+
+
 
 window.FullCalendar = Calendar;
 window.dayGridPlugin = dayGridPlugin;
 window.timeGridPlugin = timeGridPlugin;
 window.listPlugin = listPlugin;
 window.interactionPlugin = interactionPlugin;
+
+
--- resources/views/calendar/display.blade.php
+++ resources/views/calendar/display.blade.php
@@ -9,11 +9,11 @@
 
             var calendar = new FullCalendar.Calendar(calendarEl, {
                 plugins: [ dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin ],
-                initialView: 'dayGridMonth',
+                initialView: 'timeGridWeek',  // Change this line to set the week view as default
                 headerToolbar: {
                     left: 'prev,next today',
                     center: 'title',
-                    right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+                    right: 'timeGridWeek,dayGridMonth,timeGridDay,listWeek'
                 },
                 events: [
                     { title: 'Event 1', start: '2025-02-18' },
