98766f05ae83ea51fd7d9cdfe676a27943c571c1
B00143361 Amos Fakrogha <b00143361@mytudublin.ie>
2025-03-07 21:21:50 +0000

    edited appointment confirmation

--- app/Mail/AppointmentConfirmation.php
+++ app/Mail/AppointmentConfirmation.php
@@ -3,7 +3,6 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
@@ -11,14 +10,24 @@ class AppointmentConfirmation extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $clientName;
+    public $doctorName;
+    public $appointmentDate;
+    public $appointmentTime;
+    public $appointmentId;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($clientName, $doctorName, $appointmentDate, $appointmentTime, $appointmentId)
     {
-        //
+        $this->clientName = $clientName;
+        $this->doctorName = $doctorName;
+        $this->appointmentDate = $appointmentDate;
+        $this->appointmentTime = $appointmentTime;
+        $this->appointmentId = $appointmentId;
     }
 
     /**
@@ -28,6 +37,7 @@ class AppointmentConfirmation extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->subject('Appointment Confirmation')
+                   ->view('emails.appointment-confirmation');
     }
 }
