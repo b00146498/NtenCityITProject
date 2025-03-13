f98a2b1f756dd4a6a67b103ffeb1cf817136427f
b00146498@mytudublin.ie <b00146498@mytudublin.ie>
2025-03-13 22:14:01 +0000

    added new format to standard exercises table

--- resources/views/standardexercises/table.blade.php
+++ resources/views/standardexercises/table.blade.php
@@ -1,26 +1,46 @@
-<table class="table table-responsive" id="standardexercises-table">
-    <thead>
-        <th>Exercise Name</th>
-        <th>Exercise Video Link</th>
-        <th>Target Body Area</th>
-        <th colspan="3">Action</th>
-    </thead>
-    <tbody>
-    @foreach($standardexercises as $standardexercises)
-        <tr>
-            <td>{!! $standardexercises->exercise_name !!}</td>
-            <td>{!! $standardexercises->exercise_video_link !!}</td>
-            <td>{!! $standardexercises->target_body_area !!}</td>
-            <td>
-                {!! Form::open(['route' => ['standardexercises.destroy', $standardexercises->id], 'method' => 'delete']) !!}
-                <div class='btn-group'>
-                    <a href="{!! route('standardexercises.show', [$standardexercises->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-eye"></i></i></a>
-                    <a href="{!! route('standardexercises.edit', [$standardexercises->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-edit"></i></i></a>
-                    {!! Form::button('<i class="far fa-trash-alt"></i></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+<div class="container">
+    <div class="row">
+        @foreach($standardexercises as $exercise)
+        <div class="col-md-4 mb-4"> 
+            <div class="card shadow-lg">
+                <div class="card-body">
+                    <h4 class="card-title fw-bold">{{ $exercise->exercise_name }}</h4>
+                    <p class="mb-1"><strong>Target Area:</strong> {{ $exercise->target_body_area }}</p>
+
+                    @if ($exercise->exercise_video_link)
+                        <div class="embed-responsive embed-responsive-16by9 mb-2">
+                            <iframe class="embed-responsive-item" src="{{ $exercise->exercise_video_link }}" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                    @else
+                        <p class="text-muted">No video available</p>
+                    @endif
+                
+                    <div class="d-flex justify-content-center mt-3">
+                        <a href="{{ route('standardexercises.show', $exercise->id) }}" class="btn btn-outline-secondary btn-sm mx-1 d-flex align-items-center justify-content-center" style="width: 36px; height: 36px;">
+                            <i class="far fa-eye"></i>
+                        </a>
+                        <a href="{{ route('standardexercises.edit', $exercise->id) }}" class="btn btn-outline-primary btn-sm mx-1 d-flex align-items-center justify-content-center" style="width: 36px; height: 36px;">
+                            <i class="far fa-edit"></i>
+                        </a>
+                        {!! Form::open(['route' => ['standardexercises.destroy', $exercise->id], 'method' => 'delete', 'onsubmit' => "return confirm('Are you sure?')", 'class' => 'd-inline']) !!}
+                            {!! Form::button('<i class="far fa-trash-alt"></i>', [
+                                'type' => 'submit',
+                                'class' => 'btn btn-outline-danger btn-sm mx-1 d-flex align-items-center justify-content-center',
+                                'style' => 'width: 36px; height: 36px;'
+                            ]) !!}
+                        {!! Form::close() !!}
+                    </div>
                 </div>
-                {!! Form::close() !!}
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+<style>
+    h1, h2, h3, h4, h5, h6 {
+        font-weight: bold !important;
+        font-size: 2rem !important; /* Adjust size */
+    }
+</style>
+
+
