3db31ad97c16d4fc1e496482d33696324ecc8f11
B00143361 Amos Fakrogha <b00143361@mytudublin.ie>
2025-03-18 23:58:12 +0000

    fixed size again

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,50 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <div class="card">
-                <div class="card-header text-center" style="background-color: #dbb959;">
-                    <h3 class="mt-2 mb-4 text-white">My Profile</h3>
+<div class="container-fluid px-0">
+    <div class="row m-0">
+        <div class="col-12 p-0">
+            <div class="card border-0 rounded-0">
+                <div class="card-header text-center border-0" style="background-color: #dbb959; padding: 2rem 0;">
+                    <h3 class="text-white mb-4">My Profile</h3>
                     <div class="user-avatar mx-auto mb-3">
-                        <div style="width: 100px; height: 100px; background-color: #f5f5f5; border-radius: 50%; margin: 0 auto; display: flex; justify-content: center; align-items: center; box-shadow: 0 2px 5px rgba(0,0,0,0.2);">
+                        <div style="width: 100px; height: 100px; background-color: #f5f5f5; border-radius: 50%; margin: 0 auto; display: flex; justify-content: center; align-items: center;">
                             <i class="fa fa-user" style="font-size: 50px; color: #666;"></i>
                         </div>
                     </div>
                 </div>
 
-                <div class="card-body">
-                    <div class="list-group">
-                        <a href="{{ route('profile.history') }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center p-3">
+                <div class="card-body p-0">
+                    <div class="list-group rounded-0">
+                        <a href="{{ route('profile.history') }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center p-3 border-start-0 border-end-0">
                             <div>
                                 <i class="fa fa-history me-3"></i>
                                 History
                             </div>
                             <i class="fa fa-chevron-right"></i>
                         </a>
-                        <a href="{{ route('profile.personal-details') }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center p-3">
+                        <a href="{{ route('profile.personal-details') }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center p-3 border-start-0 border-end-0">
                             <div>
                                 <i class="fa fa-user me-3"></i>
                                 Personal Details
                             </div>
                             <i class="fa fa-chevron-right"></i>
                         </a>
-                        <a href="{{ route('profile.about') }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center p-3">
+                        <a href="{{ route('profile.about') }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center p-3 border-start-0 border-end-0">
                             <div>
                                 <i class="fa fa-info-circle me-3"></i>
                                 About
                             </div>
                             <i class="fa fa-chevron-right"></i>
                         </a>
-                        <a href="{{ route('profile.help') }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center p-3">
+                        <a href="{{ route('profile.help') }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center p-3 border-start-0 border-end-0">
                             <div>
                                 <i class="fa fa-question-circle me-3"></i>
                                 Help
                             </div>
                             <i class="fa fa-chevron-right"></i>
                         </a>
-                        <a href="#" onclick="confirmLogout(event)" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center p-3">
+                        <a href="#" onclick="confirmLogout(event)" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center p-3 border-start-0 border-end-0">
                             <div>
                                 <i class="fa fa-sign-out-alt me-3"></i>
                                 Log out
