23ecf91e81eea1c35278c0628fdeaed5cff11fde
B00143361 Amos Fakrogha <b00143361@mytudublin.ie>
2025-03-19 23:38:25 +0000

    added personal details route

--- routes/web.php
+++ routes/web.php
@@ -85,6 +85,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/profile/personal-details', [ProfileController::class, 'personalDetails'])->name('profile.personal-details');
     Route::get('/profile/about', [ProfileController::class, 'about'])->name('profile.about');
     Route::get('/profile/help', [ProfileController::class, 'help'])->name('profile.help');
+    Route::post('/profile/personal-details', [ProfileController::class, 'updatePersonalDetails'])->name('profile.update-details');
 });
 
 Route::get('/simple-test', function() {
