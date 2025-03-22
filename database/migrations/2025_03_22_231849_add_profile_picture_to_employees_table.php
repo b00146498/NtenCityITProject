ffa6b05c533c0809791e1ed8bb77f086a508c46f
B00143361 Amos Fakrogha <b00143361@mytudublin.ie>
2025-03-22 23:24:40 +0000

    updated add profile to employye table

--- database/migrations/2025_03_22_231849_add_profile_picture_to_employees_table.php
+++ database/migrations/2025_03_22_231849_add_profile_picture_to_employees_table.php
@@ -4,29 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddProfilePictureToEmployeesTable extends Migration
+return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('employee', function (Blueprint $table) {
-            //
+            $table->string('profile_picture')->nullable();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::table('employee', function (Blueprint $table) {
-            //
+            $table->dropColumn('profile_picture');
         });
     }
-}
+};
