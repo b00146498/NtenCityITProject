55e5472b25dc5b6a7ed541b83f72d99f9d2fdbba
b00146498@mytudublin.ie <b00146498@mytudublin.ie>
2025-03-27 11:25:11 +0000

    appoinment display works now

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Client;
+use App\Models\Employee;
+
+/*class CalendarController extends Controller
+{
+    public function display()
+    {
+        return view('calendar.display');
+    }
+}*/
+
+
 
 class CalendarController extends Controller
 {
     public function display()
     {
-        return view('calendar.display');
+        $clients = Client::all();
+        $employees = Employee::all();
+
+        return view('calendar.display', compact('clients', 'employees'));
     }
 }
  
