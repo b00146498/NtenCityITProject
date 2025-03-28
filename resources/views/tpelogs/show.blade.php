c5962ec490a07d50ef35cbb72b2a96e99079dcae
b00146498@mytudublin.ie <b00146498@mytudublin.ie>
2025-03-28 00:21:58 +0000

    added a print pdf on the workout log page

--- resources/views/tpelogs/show.blade.php
+++ resources/views/tpelogs/show.blade.php
@@ -8,13 +8,77 @@
             <a href="{{ route('tpelogs.index') }}" class="text-white text-decoration-none">✖ Close</a>
         </div>
         <div class="card-body">
-            @include('tpelogs.show_fields')
-
-            <div class="d-flex justify-content-end mt-3">
-                <a href="{!! route('tpelogs.index') !!}" class="btn btn-outline-dark px-4 py-2">Back</a>
+            <div class="d-flex justify-content-end mb-3 no-print">
+                <button onclick="window.print()" class="btn btn-warning">
+                    <i class="fas fa-print me-1"></i> Print / Save as PDF
+                </button>
             </div>
+
+            <div id="print-area">
+                @include('tpelogs.show_fields')
+            </div>
+
+            <button onclick="window.print()" class="btn btn-warning mt-3 no-print">Print / Save as PDF</button>
         </div>
     </div>
 </div>
 @endsection
+
+<style>
+@media print {
+    body, html {
+        margin: 0 !important;
+        padding: 0 !important;
+        width: 100%;
+        height: 100%;
+    }
+
+    body * {
+        visibility: hidden;
+    }
+
+    #print-area, #print-area * {
+        visibility: visible;
+    }
+
+    #print-area {
+        position: fixed;
+        left: 0;
+        top: 0;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+        background: white;
+        z-index: 9999;
+    }
+
+    .container,
+    .card,
+    .card-body,
+    .card-header {
+        all: unset;
+        display: block;
+        width: 100%;
+    }
+
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        font-size: 12pt;
+    }
+
+    th, td {
+        border: 1px solid #000;
+        padding: 10px;
+        text-align: left;
+    }
+
+    .no-print, .sidebar, nav, .navbar, footer {
+        display: none !important;
+    }
+}
+</style>
+
+
+
 
