9198fde5827bdc54b67b51ce9dc2958ba285dedb
b00146498@mytudublin.ie <b00146498@mytudublin.ie>
2025-03-31 11:50:04 +0100

    added print pdf

--- resources/views/tpelogs/show.blade.php
+++ resources/views/tpelogs/show.blade.php
@@ -1,16 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-1">
+<div class="container mt-1 print-no-margin">
     <div class="card shadow-lg">
-        <div class="card-header bg-primary text-white d-flex justify-content-between">
+        <div class="card-header bg-primary text-white d-flex justify-content-between no-print">
             <h4>View Workout Log</h4>
             <a href="{{ route('tpelogs.index') }}" class="text-white text-decoration-none">✖ Close</a>
         </div>
         <div class="card-body">
-            @include('tpelogs.show_fields')
+            <div class="d-flex justify-content-end mb-3 no-print">
+                <button onclick="window.print()" class="btn btn-warning">
+                    <i class="fas fa-print me-1"></i> Print / Save as PDF
+                </button>
+            </div>
 
-            <div class="d-flex justify-content-end mt-3">
+            <!-- ✅ Print area only -->
+            <div id="print-area">
+                @include('tpelogs.show_fields')
+            </div>
+
+            <div class="d-flex justify-content-end mt-3 no-print">
                 <a href="{!! route('tpelogs.index') !!}" class="btn btn-outline-dark px-4 py-2">Back</a>
             </div>
         </div>
@@ -18,3 +27,71 @@
 </div>
 @endsection
 
+<style>
+@media print {
+    @page {
+        size: A4 portrait;
+        margin: 10mm;
+    }
+
+    body, html {
+        margin: 0 !important;
+        padding: 0 !important;
+        width: 100%;
+        height: 100%;
+    }
+
+    body * {
+        visibility: hidden;
+    }
+
+    #print-area, #print-area * {
+        visibility: visible;
+    }
+
+    #print-area {
+        position: fixed;
+        left: 0;
+        top: 0;
+        width: 100%;
+        padding: 10px;
+        background: white;
+        z-index: 9999;
+    }
+
+    .container,
+    .card,
+    .card-body,
+    .card-header {
+        all: unset;
+        display: block;
+        width: 100%;
+    }
+
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        font-size: 10pt;
+    }
+
+    th, td {
+        border: 1px solid #000;
+        padding: 6px;
+        text-align: left;
+    }
+
+    h4, h5 {
+        font-size: 14pt;
+        margin: 5px 0;
+    }
+
+    .no-print, .sidebar, nav, .navbar, footer {
+        display: none !important;
+    }
+
+    .print-no-margin {
+        margin: 0 !important;
+        padding: 0 !important;
+    }
+}
+</style>
